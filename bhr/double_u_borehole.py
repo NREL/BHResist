fb723e4ee2aaeefbb46a1b1f18547aa8cf0ee1a4
Matt Mitchell <matt.mitchell@nrel.gov>
2025-01-07 20:18:51 -0700

    format

--- bhr/double_u_borehole.py
+++ bhr/double_u_borehole.py
@@ -1,26 +1,24 @@
 from math import pi, log
 
 from bhr.u_tube import UTube
-
 from bhr.utilities import coth
 
-#math functions
+# math functions
 ln = log
-
 
 
 class DoubleUTube(UTube):
 
     def __init__(self,
-                 borehole_diameter: float, #m
-                 pipe_outer_diameter: float, #m
-                 pipe_dimension_ratio: float, #unitless, ratio of pipe outer diameter / thickness
-                 length: float, #m
-                 shank_space: float, # radial distance between pipe centers, assumes radialy symetrical placement
-                 pipe_conductivity: float, # W/(m-K)
+                 borehole_diameter: float,  # m
+                 pipe_outer_diameter: float,  # m
+                 pipe_dimension_ratio: float,  # unitless, ratio of pipe outer diameter / thickness
+                 length: float,  # m
+                 shank_space: float,  # radial distance between pipe centers, assumes radially symmetrical placement
+                 pipe_conductivity: float,  # W/(m-K)
                  pipe_config: str,
-                 grout_conductivity: float, #  W/(m-K)
-                 soil_conductivity: float, # W/(m-K)
+                 grout_conductivity: float,  # W/(m-K)
+                 soil_conductivity: float,  # W/(m-K)
                  fluid_type: str,
                  fluid_concentration: float = 0):
 
@@ -29,25 +27,25 @@ class DoubleUTube(UTube):
 
         # static parameters
         self.grout_conductivity = grout_conductivity
-        self.borehole_radius = borehole_diameter / 2 * 1000 # radius of borehole (mm) rb
-        self.pipe_radius = pipe_outer_diameter / 2  * 1000 # pipe outer radius (mm) rp
+        self.borehole_radius = borehole_diameter / 2 * 1000  # radius of borehole (mm) rb
+        self.pipe_radius = pipe_outer_diameter / 2 * 1000  # pipe outer radius (mm) rp
         self.pipe_config = pipe_config
         self.bh_length = length  # length of borehole is half the length of one pipe (m)
         self.grout_conductivity = grout_conductivity
-        self.soil_conductivity = soil_conductivity #W/(m-K)
-        self.shank_space = shank_space * 1000 # (mm) radial distance between centers of symmetrically placed pipes and borehole center rc
+        self.soil_conductivity = soil_conductivity  # W/(m-K)
+        self.shank_space = shank_space * 1000  # (mm) radial distance between centers of symmetrically placed pipes and borehole center rc
         self.sigma = (self.grout_conductivity - self.soil_conductivity) / (
                 self.grout_conductivity + self.soil_conductivity)
 
         # -derived variables-
         self.Ppc = self.pipe_radius ** 2 / (4 * self.shank_space ** 2)  # dimensionless parameter
         self.Pc = self.shank_space ** 2 / (self.borehole_radius ** 8 - self.shank_space ** 8) ** (
-                    1 / 4)  # dimensionless parameter
+                1 / 4)  # dimensionless parameter
         self.Pb = self.borehole_radius ** 2 / (self.borehole_radius ** 8 - self.shank_space ** 8) ** (
-                    1 / 4)  # dimensionless parameter
+                1 / 4)  # dimensionless parameter
 
         self.sigma = (grout_conductivity - soil_conductivity) / (
-                    grout_conductivity + soil_conductivity)  # thermal conductivity ratio, dimensionless
+                grout_conductivity + soil_conductivity)  # thermal conductivity ratio, dimensionless
 
         # non-static parameters
         self.beta = None
@@ -56,11 +54,12 @@ class DoubleUTube(UTube):
         self.Rb_eff_UHF = None
         self.Rb_eff_UWT = None
         self.eff_bhr_ave = None
+        self.pipe_resist = None
 
     def update_b1(self,
-                    flow_rate: float = None,
-                    temperature: float = None,
-                    pipe_resist: float = None) -> float:
+                  flow_rate: float = None,
+                  temperature: float = None,
+                  pipe_resist: float = None) -> float:
         """
         Updates Beta coefficient.
 
@@ -90,17 +89,21 @@ class DoubleUTube(UTube):
 
         return self.b1
 
-    def calc_bh_resist(self, flow_rate, temperature,pipe_resist):
+    def calc_bh_resist(self, flow_rate, temperature, pipe_resist):
         """
-        Calculates tube-to-borehole resistance (aka local borehole resistnance) .
+        Calculates tube-to-borehole resistance (aka local borehole resistance) .
 
         Claesson, Johan, and Saqib Javed. 2019. “Explicit Multipole Formulas
         and Thermal Network Models for Calculating Thermal Resistances of
         Double U-Pipe Borehole Heat Exchangers.” Science and Technology for
-         the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
+        the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
 
-         eqns.13 & 14
-         """
+        Eq: 13 & 14
+
+        :param flow_rate: mass flow rate, kg/s
+        :param temperature: temperature, Celsius
+        :param pipe_resist: pipe conduction and convection resistance, K/(W/m). only used for testing
+        """
 
         self.update_b1(flow_rate, temperature, pipe_resist)
 
@@ -111,10 +114,10 @@ class DoubleUTube(UTube):
 
         # --Borehole resistance, 1st order [K/(W/m)]--
         self.Rb1 = Rb0 - 1 / (8 * pi * self.grout_conductivity) * (
-                    self.b1 * self.Ppc * (3 - 8 * self.sigma * self.Pc ** 4) ** 2
-                    ) / (1 + self.b1 * self.Ppc * (5 + 64 * self.sigma * self.Pc ** 4 * self.Pb ** 4))
+                self.b1 * self.Ppc * (3 - 8 * self.sigma * self.Pc ** 4) ** 2
+        ) / (1 + self.b1 * self.Ppc * (5 + 64 * self.sigma * self.Pc ** 4 * self.Pb ** 4))
 
-        #debugging statements
+        # debugging statements
         # print("Lambda_b = %.3E" % self.grout_conductivity)
         # print("Lambda_soil = %.3E" % self.soil_conductivity)
         # print("Length = %.3E" % self.bh_length)
@@ -127,32 +130,36 @@ class DoubleUTube(UTube):
 
         return self.Rb1
 
-    def calc_internal_resist_pipe(self, flow_rate, temperature,pipe_resist):
+    def calc_internal_resist_pipe(self, flow_rate, temperature, pipe_resist):
         """
         Calculates tube-to-tube resistance (aka internal resistance).
 
         Claesson, Johan, and Saqib Javed. 2019. “Explicit Multipole Formulas
         and Thermal Network Models for Calculating Thermal Resistances of
         Double U-Pipe Borehole Heat Exchangers.” Science and Technology for
-         the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
+        the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
 
-         eqns. 18, 19, 22, 23
-         """
+        Eq: 18, 19, 22, 23
+
+        :param flow_rate: mass flow rate, kg/s
+        :param temperature: temperature, Celsius
+        :param pipe_resist: pipe conduction and convection resistance, K/(W/m). only used for testing
+        """
 
         self.update_b1(flow_rate, temperature, pipe_resist)
 
         if self.pipe_config == "DIAGONAL":
             # 0th order
             Ra0 = 2 * self.pipe_resist + 2 / (2 * pi * self.grout_conductivity) * (
-                        ln(self.shank_space / self.pipe_radius) +
-                        self.sigma * ln((self.borehole_radius ** 4 + self.shank_space ** 4) / (
-                            self.borehole_radius ** 4 - self.shank_space ** 4)))
+                    ln(self.shank_space / self.pipe_radius) +
+                    self.sigma * ln((self.borehole_radius ** 4 + self.shank_space ** 4) / (
+                    self.borehole_radius ** 4 - self.shank_space ** 4)))
 
             # 1st order
             self.Ra1 = Ra0 - 2 / (2 * pi * self.grout_conductivity) * (
-                        self.b1 * self.Ppc * (1 + 8 * self.sigma * self.Pc ** 2 * self.Pb ** 2) ** 2
-                        ) / (1 - self.b1 * self.Ppc * (
-                        3 - 32 * self.sigma * (self.Pc ** 2 * self.Pb ** 6 + self.Pc ** 6 * self.Pb ** 2)))
+                    self.b1 * self.Ppc * (1 + 8 * self.sigma * self.Pc ** 2 * self.Pb ** 2) ** 2
+            ) / (1 - self.b1 * self.Ppc * (
+                    3 - 32 * self.sigma * (self.Pc ** 2 * self.Pb ** 6 + self.Pc ** 6 * self.Pb ** 2)))
 
             print("Rad0 = %.3E" % Ra0)
             print("Rad1 = %.3E" % self.Ra1)
@@ -162,24 +169,24 @@ class DoubleUTube(UTube):
         elif self.pipe_config == "ADJACENT":
             # 0th order
             Ra0 = 2 * self.pipe_resist + 2 / (2 * pi * self.grout_conductivity) * (
-                        ln(2 * self.shank_space / self.pipe_radius) +
-                        self.sigma * ln((self.borehole_radius ** 2 + self.shank_space ** 2) / (
-                            self.borehole_radius ** 2 - self.shank_space ** 2)))
+                    ln(2 * self.shank_space / self.pipe_radius) +
+                    self.sigma * ln((self.borehole_radius ** 2 + self.shank_space ** 2) / (
+                    self.borehole_radius ** 2 - self.shank_space ** 2)))
 
             # 1st order
             M11 = 1 + 16 * self.b1 * self.sigma * self.Ppc * (
-                        3 * self.Pc ** 3 * self.Pb ** 5 + self.Pc ** 7 * self.Pb)  # matrix variable
+                    3 * self.Pc ** 3 * self.Pb ** 5 + self.Pc ** 7 * self.Pb)  # matrix variable
             M22 = -1 - 16 * self.b1 * self.sigma * self.Ppc * (
-                        self.Pc * self.Pb ** 7 + 3 * self.Pc ** 5 * self.Pb ** 3)  # matrix variable
+                    self.Pc * self.Pb ** 7 + 3 * self.Pc ** 5 * self.Pb ** 3)  # matrix variable
             M21 = self.b1 * self.Ppc  # matrix variable
             V1 = 1 - 8 * self.sigma * self.Pc ** 3 * self.Pb  # vector variable
             V2 = 3 + 8 * self.sigma * self.Pc * self.Pb ** 3  # vector variable
 
             self.Ra1 = Ra0 + 2 / (2 * pi * self.grout_conductivity) * self.b1 * self.Ppc / 2 * (
-                        V2 ** 2 * M11 - 2 * V1 * V2 * M21 -
-                        V1 ** 2 * M22) / (M11 * M22 + M21 ** 2)
+                    V2 ** 2 * M11 - 2 * V1 * V2 * M21 -
+                    V1 ** 2 * M22) / (M11 * M22 + M21 ** 2)
 
-            #degbugging print statements
+            # degbugging print statements
             print("Rad0 = %.3E" % Ra0)
             print("Rad1 = %.3E" % self.Ra1)
 
@@ -189,16 +196,21 @@ class DoubleUTube(UTube):
 
     def calc_effective_bh_resist_uhf(self, flow_rate, temperature):
         """
-       Calculates effective borehole resistance for uniform heat flux along the borehole.
+        Calculates effective borehole resistance for uniform heat flux along the borehole.
 
-       Claesson, Johan, and Saqib Javed. 2019. “Explicit Multipole Formulas
-       and Thermal Network Models for Calculating Thermal Resistances of
-       Double U-Pipe Borehole Heat Exchangers.” Science and Technology for
+        Claesson, Johan, and Saqib Javed. 2019. “Explicit Multipole Formulas
+        and Thermal Network Models for Calculating Thermal Resistances of
+        Double U-Pipe Borehole Heat Exchangers.” Science and Technology for
         the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
 
-        eq. 44
+        Eq: 44
+
+        :param flow_rate: mass flow rate, kg/s
+        :param temperature: temperature, Celsius
+        # :param pipe_resist: pipe conduction and convection resistance, K/(W/m). only used for testing
         """
-        Rv = self.bh_length / ( self.fluid.cp(temperature) * flow_rate)  # (K/(w/m)) thermal resistance factor
+
+        Rv = self.bh_length / (self.fluid.cp(temperature) * flow_rate)  # (K/(w/m)) thermal resistance factor
 
         self.Rb_eff_UHF = self.Rb1 + Rv ** 2 / (6 * self.Ra1)
 
@@ -214,21 +226,30 @@ class DoubleUTube(UTube):
         Claesson, Johan, and Saqib Javed. 2019. “Explicit Multipole Formulas
         and Thermal Network Models for Calculating Thermal Resistances of
         Double U-Pipe Borehole Heat Exchangers.” Science and Technology for
-         the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
+        the Built Environment 25 (8): 980–92. doi:10.1080/23744731.2019.1620565.
 
-         eq. 46
-         """
+        Eq: 46
+
+        :param flow_rate: mass flow rate, kg/s
+        :param temperature: temperature, Celsius
+        # :param pipe_resist: pipe conduction and convection resistance, K/(W/m). only used for testing
+        """
+
         Rv = self.bh_length / (flow_rate * self.fluid.cp(temperature))  # (K/(w/m)) thermal resistance factor
         n = Rv / (2 * self.Rb1 * self.Ra1) ** (1 / 2)
         self.Rb_eff_UWT = self.Rb1 * n * coth(n)
 
-        #print("Ra_eff_UWT = %.3E\n" % self.Rb_eff_UWT)
+        # print("Ra_eff_UWT = %.3E\n" % self.Rb_eff_UWT)
 
         return self.Rb_eff_UWT
 
     def calc_effective_bh_resist_ave(self):
-        """ Averages effective borehole resistance results for the two boundry conditions
-        Uniform Heat Flux and Uniform Borehole Wall temperature
         """
+        Averages effective borehole resistance results for the two boundary conditions
+        Uniform Heat Flux and Uniform Borehole Wall temperature
+
+        No arguments??
+        """
+
         self.eff_bhr_ave = (self.Rb_eff_UHF + self.Rb_eff_UWT) / 2.0
         return self.eff_bhr_ave
--- bhr/tests/test_double_u_borehole.py
+++ bhr/tests/test_double_u_borehole.py
@@ -12,11 +12,11 @@ class TestDoubleUBorehole(TestCase):
             "length": 200,
             "shank_space": 0.02263,
             "pipe_conductivity": 0.389,
-            "pipe_config" :"DIAGONAL",
+            "pipe_config": "DIAGONAL",
             "grout_conductivity": 1.5,
             "soil_conductivity": 3,
             "fluid_type": "WATER"
-            }
+        }
 
     def test_init(self):
         bh = DoubleUTube(**self.inputs)
@@ -29,14 +29,14 @@ class TestDoubleUBorehole(TestCase):
         flow_rate = 0.2077
         temperature = 20
         pipe_resist = 0.05
-        #the following tests rely on each previous test
+        # the following tests rely on each previous test
         self.assertAlmostEqual(bh.update_b1(flow_rate, temperature, pipe_resist), 0.359, delta=tolerance)
-        self.assertAlmostEqual(bh.calc_bh_resist(flow_rate, temperature,pipe_resist), 7.509E-02, delta=tolerance)
-        self.assertAlmostEqual(bh.calc_internal_resist_pipe(flow_rate, temperature,pipe_resist), 0.1604, delta=tolerance)
+        self.assertAlmostEqual(bh.calc_bh_resist(flow_rate, temperature, pipe_resist), 7.509E-02, delta=tolerance)
+        self.assertAlmostEqual(bh.calc_internal_resist_pipe(flow_rate, temperature, pipe_resist), 0.1604,
+                               delta=tolerance)
         self.assertAlmostEqual(bh.calc_effective_bh_resist_uhf(flow_rate, temperature), 0.1302, delta=tolerance)
         self.assertAlmostEqual(bh.calc_effective_bh_resist_ubwt(flow_rate, temperature), 0.1235, delta=tolerance)
         self.assertAlmostEqual(bh.calc_effective_bh_resist_ave(), 0.1269, delta=tolerance)
-
 
     def test_calc_resistances_Adjacent(self):
         self.inputs.update({"pipe_config": "ADJACENT"})
@@ -47,12 +47,11 @@ class TestDoubleUBorehole(TestCase):
         flow_rate = 0.2077
         temperature = 20
         pipe_resist = 0.05
-        #the following tests rely on each previous test
+        # the following tests rely on each previous test
         self.assertAlmostEqual(bh.update_b1(flow_rate, temperature, pipe_resist), 0.359, delta=tolerance)
-        self.assertAlmostEqual(bh.calc_bh_resist(flow_rate, temperature,pipe_resist), 7.509E-02, delta=tolerance)
-        self.assertAlmostEqual(bh.calc_internal_resist_pipe(flow_rate, temperature,pipe_resist), 0.2617, delta=tolerance)
+        self.assertAlmostEqual(bh.calc_bh_resist(flow_rate, temperature, pipe_resist), 7.509E-02, delta=tolerance)
+        self.assertAlmostEqual(bh.calc_internal_resist_pipe(flow_rate, temperature, pipe_resist), 0.2617,
+                               delta=tolerance)
         self.assertAlmostEqual(bh.calc_effective_bh_resist_uhf(flow_rate, temperature), 0.1089, delta=tolerance)
         self.assertAlmostEqual(bh.calc_effective_bh_resist_ubwt(flow_rate, temperature), 0.1062, delta=tolerance)
         self.assertAlmostEqual(bh.calc_effective_bh_resist_ave(), 0.1075, delta=tolerance)
-
-
